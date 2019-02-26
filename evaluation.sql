b28c5ab1c0a2906e44ca5b7b135c91164dbd89ed
vsolondev <vsolondev@gmail.com>
2019-02-26 23:18:00 +0800

    updated

--- evaluation.sql
+++ evaluation.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 26, 2019 at 03:48 PM
+-- Generation Time: Feb 26, 2019 at 04:17 PM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.2.12
 
